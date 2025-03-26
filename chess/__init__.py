76158fdb2324d3f883d34910c52567ea46b09cb6
VarvaraMoya <maslovavarvara08@gmail.com>
2025-03-26 22:52:28 +0300

    Create __init__.py

---
+++ chess/__init__.py
@@ -0,0 +1,5 @@
+from .board import ChessBoard, ModifiedChessBoard
+from .game import ChessGame, ModifiedChessGame
+from .pieces import *
+
+__all__ = ['ChessBoard', 'ModifiedChessBoard', 'ChessGame', 'ModifiedChessGame']
