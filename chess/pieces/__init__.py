4bf501a816033933092f5905f1d11c2b24a1b6ba
VarvaraMoya <maslovavarvara08@gmail.com>
2025-03-26 22:41:19 +0300

    Create __init__.py

---
+++ chess/pieces/__init__.py
@@ -0,0 +1,15 @@
+from .base import ChessPiece
+from .king import King
+from .queen import Queen
+from .rook import Rook
+from .bishop import Bishop
+from .knight import Knight
+from .pawn import Pawn
+from .wizard import Wizard
+from .dragon import Dragon
+from .jester import Jester
+
+__all__ = [
+    'ChessPiece', 'King', 'Queen', 'Rook',
+    'Bishop', 'Knight', 'Pawn', 'Wizard',
+    'Dragon', 'Jester'
